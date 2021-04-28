5d290eadb0fbe4a8e97a2857877fc854f8e47b7b
y4shiro <y4shironao@gmail.com>
2021-04-28 20:45:46 +0900

    useState でフォームの入力値を管理

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 const App: React.FC = () => {
+  const [text, setText] = useState("");
+
   return (
     <div>
       <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" value={""} onChange={(e) => e.preventDefault()} />
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
         <input
           type="submit"
           value="追加"

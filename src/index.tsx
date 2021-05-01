6f268698f7dfecd6c16fe09c1dfd874f84f2e358
y4shiro <y4shironao@gmail.com>
2021-05-01 10:58:14 +0900

    Material-UI theme.js 追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
 import App from "./App";
+import theme from "./theme";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+  document.querySelector("#root")
+);
